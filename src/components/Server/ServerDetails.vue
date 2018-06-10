77a497616ce0e961677db9109655b8f4b9214b87
adam <adam@whitespark.ca>
2018-06-09 22:41:15 -0700

    first commit

---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">No server selected.</p>
+        <p v-else>Server ID: {{ server.id }} Server Status: {{ server.status }}</p>
+        <hr>
+        <button @click="resetStatus">Change to Normal</button>
+    </div>
+</template>
+
+<script>
+    import { serverBus } from '../../main';
+
+    export default {
+        data: function() {
+            return {
+                server: null,
+            };
+        },
+        methods: {
+            resetStatus() {
+                this.server.status = 'Normal';
+            },
+        },
+        created() {
+            serverBus.$on('serverSelected', (server) => {
+                console.log('SERVER ID', server);
+                this.server = server;
+            });
+        },
+    };
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Server/Servers.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <app-server
+                v-for="(server, i) in servers"
+                :key="`server-${i}`"
+                :server="server"></app-server>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import appServer from './Server.vue'
+
+    export default {
+        components: {
+            'app-server': appServer,
+        },
+        data: function() {
+            return {
+                servers: [
+                    { id: 1, status: 'Normal' },
+                    { id: 2, status: 'Critical' },
+                    { id: 3, status: 'Offline' },
+                    { id: 4, status: 'Normal' },
+                ],
+            };
+        },
+        methods: {
+            onShowServerDetails() {
+                console.log('KLAJSH')
+            },
+        },
+    };
+</script>
+
+<style>
+
+</style>
